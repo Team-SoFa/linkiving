7a71e49e7ea93cb3e0fe6c58364bb15a7b9b36bc
Bangdayeon <dybang00@gmail.com>
2025-05-29 20:14:19 +0900

    Chore: set up folder structure (#20)

---
+++ src/app/login/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <main>
+            <p> This is the content of the login.</p>
+        </main>
+    );
+}
---
+++ src/app/my-info/MyInfo.tsx
@@ -0,0 +1,7 @@
+export default function MyInfo() {
+    return (
+        <main>
+            <p> This is the content of the my-info.</p>
+        </main>
+    );
+}
