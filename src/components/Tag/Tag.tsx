4dd76831378efb3c6b027ab107a6d88b93e82ce0
Bangdayeon <dybang00@gmail.com>
2025-07-18 18:26:28 +0900

    Feat: add tag and taglist component (#71)

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+
+import IconButton from '../IconButton/IconButton';
+
+interface TagProps {
+  label: string;
+  onDelete?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Tag({ label, onDelete }: TagProps) {
+  return (
+    <div className="inline-flex cursor-default items-center justify-center gap-1 rounded-md bg-gray-300 px-2 py-1 text-xs font-medium text-gray-900">
+      <span>{label}</span>
+      {onDelete && (
+        <IconButton
+          icon="IC_Close"
+          variant="ghost"
+          size="sm"
+          ariaLabel={label || `${label} 태그 삭제`}
+          onClick={onDelete}
+        />
+      )}
+    </div>
+  );
+}
