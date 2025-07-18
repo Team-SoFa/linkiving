dd90e6b3ec8d9b70c55a7ccd866ca186bfae3768
Bangdayeon <dybang00@gmail.com>
2025-07-19 00:09:34 +0900

    Feat: add modal component (#74)

---
+++ src/components/Modal/ModalProvider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useModalStore } from '@/stores/modalStore';
+
+import Ex1 from './Ex1';
+import Ex2 from './Ex2';
+import ModalWrapper from './ModalWrapper';
+
+export default function ModalProvider() {
+  const { isOpen, type, close } = useModalStore();
+
+  if (!isOpen) return null;
+
+  let content;
+  switch (type) {
+    case 'ex1':
+      content = <Ex1 />;
+      break;
+    case 'ex2':
+      content = <Ex2 />;
+      break;
+    default:
+      content = null;
+  }
+
+  return <ModalWrapper onClose={close}>{content}</ModalWrapper>;
+}
