e4f790237ad34c33956c7afd36b2559c0063ab69
Bangdayeon <dybang00@gmail.com>
2025-07-19 00:09:34 +0900

    Feat: add modal component (#74)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import ReactQueryProvider from "@/components/ReactQueryProvider";
+import ModalProvider from "@/components/Modal/ModalProvider";
 
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReactQueryProvider>
+          <ModalProvider />
           {children}
         </ReactQueryProvider>
       </body>
