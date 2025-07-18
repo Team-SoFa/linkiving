14b28d006f5685c7f9ce51b355f6e46040227a26
Bangdayeon <dybang00@gmail.com>
2025-07-18 22:55:10 +0900

    Feat: add header component (#79)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,20 @@
-// header component dummy
+'use client';
+
+import React from 'react';
+
+import Button from '../Button/Button';
+
+export default function Header() {
+  const headerStyle = 'bg-blue-200 p-4 flex justify-between w-full';
+  const setStyle = 'gap-2 flex items-center';
+
+  return (
+    <header className={headerStyle}>
+      <Button>BTN</Button>
+      <div className={setStyle}>
+        <Button>더보기</Button>
+        <Button>로그인</Button>
+      </div>
+    </header>
+  );
+}
