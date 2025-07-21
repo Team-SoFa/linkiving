9131da654b8ea96753e24015e4e13e4d68400b0e
Bangdayeon <dybang00@gmail.com>
2025-07-21 21:09:07 +0900

    Feat: add footer component (#80)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,1 +1,10 @@
-// footer component dummy
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="flex h-40 items-end justify-center bg-gray-800 p-5 text-center text-sm text-gray-500 dark:text-gray-400">
+      <p>&copy; {new Date().getFullYear()} Linkiving. All rights reserved.</p>
+    </footer>
+  );
+};
+export default Footer;
