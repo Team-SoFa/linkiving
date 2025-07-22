2812e81abf533974d207a3f96b82be42fb94b46a
Bangdayeon <dybang00@gmail.com>
2025-07-22 16:46:23 +0900

    Feat: add bookmarkcard component (#67)

---
+++ src/components/BookmarkCard/BookmarkCard.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import BookmarkCard from "./BookmarkCard";
+
+const meta: Meta<typeof BookmarkCard> = {
+    title: "Components/BookmarkCard",
+    component: BookmarkCard,
+    tags: ["autodocs"],
+    argTypes: {
+        imageUrl: { control: "text" },
+        title: { control: "text" },
+        summary: { control: "text" },
+        link: { control: "text" },
+        onClick: { action: "open details panel"}
+    },
+};
+export default meta;
+
+type Story = StoryObj<typeof BookmarkCard>;
+
+export const Default: Story = {
+    args: {
+        imageUrl: "/globe.svg",
+        title: "Link Title",
+        summary: "This is a sample bookmark card with a placeholder image.",
+        link: "https://naver.com"
+    },
+};
