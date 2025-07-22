941da851609d9ad4f3cb51ad94fe4cbafbdafa07
Bangdayeon <dybang00@gmail.com>
2025-07-22 16:46:23 +0900

    Feat: add bookmarkcard component (#67)

--- src/components/basics/Anchor/Anchor.style.ts
+++ src/components/basics/Anchor/Anchor.style.ts
@@ -1,7 +1,7 @@
 import { tv } from 'tailwind-variants';
 
 export const style = tv({
-  base: 'text-anchor inline-flex items-center justify-center gap-1 hover:underline [&>svg]:text-inherit [&>svg]:transition-colors [&>svg]:duration-150 [&>svg]:ease-in-out',
+  base: 'text-anchor flex items-center gap-1 hover:underline [&>svg]:text-inherit [&>svg]:transition-colors [&>svg]:duration-150 [&>svg]:ease-in-out',
   variants: {
     size: {
       sm: 'font-anchor-sm',
