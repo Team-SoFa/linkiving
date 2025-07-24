ae4fcb6796a2cbe781e178556d048cd3d3e77339
Bangdayeon <dybang00@gmail.com>
2025-07-25 06:11:58 +0900

    Fix: modify button component (#99)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,64 +2,93 @@
 
 import { clsx } from 'clsx';
 import React from 'react';
+import { tv } from 'tailwind-variants';
 
-interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  type?: 'button' | 'submit' | 'reset';
-  variant?: 'primary' | 'outline' | 'ghost';
-  size?: 'sm' | 'md' | 'lg';
-  disabled?: boolean;
-  icon?: React.ReactNode;
+const styles = tv({
+  base: 'inline-flex cursor-pointer items-center rounded font-medium transition outline-none',
+  variants: {
+    variant: {
+      primary: 'bg-gray-900 text-white hover:bg-gray-800',
+      secondary: 'bg-gray-50 text-gray-900 hover:bg-blue-50',
+      ghost: 'bg-gray-50 text-gray-500 hover:bg-blue-50 hover:text-gray-700',
+    },
+    size: {
+      sm: 'px-4 py-2 text-[12px] [&_svg]:h-4 [&_svg]:w-4',
+      md: 'px-5 py-[10px] text-[14px] [&_svg]:h-5 [&_svg]:w-5',
+      lg: 'px-6 py-3 text-[16px] [&_svg]:h-6 [&_svg]:w-6',
+    },
+    radius: {
+      sm: 'rounded-[6px]',
+      md: 'rounded-[8px]',
+      lg: 'rounded-[12px]',
+      full: 'rounded-full',
+    },
+    disabled: {
+      true: 'pointer-events-none bg-gray-400 opacity-50',
+      false: '',
+    },
+  },
+});
+
+// 많이 해보면서 머리에 넣는 느낌
+// HTML 000 Element
+// 각 태그들이 가진 액션 (ClickEvent, MouseEvent 등도 ) 같이 알아두자
+export interface ButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type' | 'disabled' | 'onClick'> {
+  className?: string;
+  label?: React.ReactNode;
+  icon?: React.ReactElement<React.SVGProps<SVGSVGElement>>;
   iconPosition?: 'left' | 'right';
+  type?: 'button' | 'submit' | 'reset';
+  variant?: 'primary' | 'secondary' | 'ghost';
+  size?: 'sm' | 'md' | 'lg';
+  radius?: 'sm' | 'md' | 'lg' | 'full';
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>; // HTMLButtonElement 타입에서 발생할 수 있는 MouseEventHandler타입
 }
 
-export default function Button({
-  children,
-  variant = 'primary',
-  onClick,
-  type = 'button',
-  size = 'md',
-  icon,
-  iconPosition = 'left',
-  disabled,
-}: ButtonProps) {
-  const baseStyle = 'inline-flex items-center outline-none rounded font-medium transition';
-  const variants = {
-    primary: 'bg-blue-500 text-white hover:bg-gray-800 cursor-pointer',
-    outline: 'border border-gray-300 text-gray-700 hover:bg-gray-100 cursor-pointer',
-    ghost: 'text-gray-700 hover:bg-gray-100 cursor-pointer',
-  };
-  const sizes = {
-    sm: 'px-3 py-1.5 text-sm',
-    md: 'px-4 py-2 text-base',
-    lg: 'px-5 py-3 text-lg',
-  };
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      label,
+      icon = null,
+      iconPosition = 'left',
+      type = 'button',
+      variant = 'primary',
+      size = 'md',
+      radius = 'md',
+      disabled,
+      onClick,
+      ...rest
+    },
+    ref
+  ) => {
+    const classes = styles({
+      variant,
+      size,
+      radius,
+      disabled,
+    });
 
-  // 클래스 조합부
-  const classes = clsx(baseStyle, variants[variant], sizes[size], {
-    'cursor-not-allowed bg-gray-400 opacity-50': disabled,
-    'hover:cursor-not-allowed hover:bg-gray-400 hover:opacity-50': disabled,
-  });
+    return (
+      <button
+        ref={ref}
+        className={clsx(classes, className)}
+        aria-disabled={disabled}
+        disabled={disabled}
+        type={type}
+        onClick={onClick}
+        {...rest}
+      >
+        {icon && iconPosition === 'left' && <span className="mr-2">{icon}</span>}
+        {label}
+        {icon && iconPosition === 'right' && <span className="ml-2">{icon}</span>}
+      </button>
+    );
+  }
+);
 
-  // 버튼 실제 출력 부분
-  const ButtonContent = (
-    <>
-      {icon && iconPosition === 'left' && <span className="mr-2">{icon}</span>}
-      {children}
-      {icon && iconPosition === 'right' && <span className="ml-2">{icon}</span>}
-    </>
-  );
+Button.displayName = 'Button';
 
-  return (
-    <button
-      type={type}
-      className={classes}
-      disabled={disabled}
-      aria-disabled={disabled}
-      onClick={onClick}
-    >
-      {ButtonContent}
-    </button>
-  );
-}
+export default Button;
