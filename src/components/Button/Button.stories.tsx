aef4c1f759a188c9745b61e2d53c3b739b8ed0a7
Bangdayeon <dybang00@gmail.com>
2025-07-25 06:11:58 +0900

    Fix: modify button component (#99)

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,71 +1,61 @@
 // components/ui/Button.stories.tsx
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { IconMap } from '../Icons/icons';
 import Button from './Button';
 
-const meta: Meta<typeof Button> = {
+const ICONS = Object.keys(IconMap);
+
+const meta = {
   title: 'Components/Button',
   component: Button,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: '[텍스트] 및 [아이콘 포함 텍스트], children을 표시하는 버튼입니다.',
+      },
+    },
+  },
   argTypes: {
+    label: {
+      control: 'text',
+      table: { type: { summary: 'string' } },
+    },
     variant: {
-      control: 'select',
+      control: 'inline-radio',
       options: ['primary', 'outline', 'ghost'],
+      table: { type: { summary: 'string' } },
     },
     size: {
-      control: 'select',
+      control: 'inline-radio',
       options: ['sm', 'md', 'lg'],
+      table: { type: { summary: 'string' } },
+    },
+    icon: {
+      options: [undefined, ...ICONS],
+      table: { type: { summary: 'IconMapTypes' } },
     },
     iconPosition: {
       control: 'inline-radio',
       options: ['left', 'right'],
+      table: { type: { summary: 'string' } },
     },
-    icon: {
-      control: false,
-    },
-    onClick: { action: 'clicked' },
+    type: { table: { disable: true } },
+    className: { table: { disable: true } },
+    onClick: { action: 'clicked', table: { disable: true } },
   },
-};
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    children: 'Primary Button',
+    label: 'Primary Button',
     variant: 'primary',
     size: 'md',
-    icon: '✈️',
     iconPosition: 'left',
-  },
-};
-
-export const Outline: Story = {
-  args: {
-    children: 'Outline Button',
-    variant: 'outline',
-    size: 'md',
-    icon: '✈️',
-    iconPosition: 'left',
-  },
-};
-
-export const Ghost: Story = {
-  args: {
-    children: 'Ghost Button',
-    variant: 'ghost',
-    size: 'md',
-    icon: '✈️',
-    iconPosition: 'left',
-  },
-};
-
-export const Disabled: Story = {
-  args: {
-    children: 'Disabled',
-    variant: 'ghost',
-    size: 'md',
-    disabled: true,
-    icon: '✈️',
-    iconPosition: 'left',
+    disabled: false,
   },
 };
