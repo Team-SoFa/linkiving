bcbdee049652419753f36a9516ef8b33c203f76b
Bangdayeon <dybang00@gmail.com>
2025-08-01 15:42:25 +0900

    Chore: eslint setting (#109)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,70 +3,65 @@
 import React from "react";
 import { clsx } from "clsx";
 
-
 interface ButtonProps {
-    children: React.ReactNode;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
-    type?: "button" | "submit" | "reset";
-    variant?: "primary" | "outline" | "ghost";
-    size?: "sm" | "md" | "lg";
-    disabled?: boolean;
-    icon?: React.ReactNode
-    iconPosition?: "left" | "right"
+  children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "button" | "submit" | "reset";
+  variant?: "primary" | "outline" | "ghost";
+  size?: "sm" | "md" | "lg";
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
 }
 
 export default function Button({
-    children,
-    variant = "primary",
-    onClick,
-    type = "button",
-    size = "md",
-    icon,
-    iconPosition = "left",
-    disabled,
-    ...props
+  children,
+  variant = "primary",
+  onClick,
+  type = "button",
+  size = "md",
+  icon,
+  iconPosition = "left",
+  disabled,
 }: ButtonProps) {
-    const baseStyle = "inline-flex items-center outline-none rounded font-medium transition"
-    const variants = {
-        primary: "bg-blue-500 text-white hover:bg-gray-800 cursor-pointer",
-        outline: "border border-gray-300 text-gray-700 hover:bg-gray-100 cursor-pointer",
-        ghost: "text-gray-700 hover:bg-gray-100 cursor-pointer"
-    }
-    const sizes = {
-        sm: "px-3 py-1.5 text-sm",
-        md: "px-4 py-2 text-base",
-        lg: "px-5 py-3 text-lg"
-    }
+  const baseStyle =
+    "inline-flex items-center outline-none rounded font-medium transition";
+  const variants = {
+    primary: "bg-blue-500 text-white hover:bg-gray-800 cursor-pointer",
+    outline:
+      "border border-gray-300 text-gray-700 hover:bg-gray-100 cursor-pointer",
+    ghost: "text-gray-700 hover:bg-gray-100 cursor-pointer",
+  };
+  const sizes = {
+    sm: "px-3 py-1.5 text-sm",
+    md: "px-4 py-2 text-base",
+    lg: "px-5 py-3 text-lg",
+  };
 
-    // 클래스 조합부
-    const classes = clsx(
-        baseStyle,
-        variants[variant],
-        sizes[size],
-        {
-            "bg-gray-400 opacity-50 cursor-not-allowed": disabled,
-            "hover:bg-gray-400 hover:opacity-50 hover:cursor-not-allowed": disabled
-        }
-    )
+  // 클래스 조합부
+  const classes = clsx(baseStyle, variants[variant], sizes[size], {
+    "bg-gray-400 opacity-50 cursor-not-allowed": disabled,
+    "hover:bg-gray-400 hover:opacity-50 hover:cursor-not-allowed": disabled,
+  });
 
-    // 버튼 실제 출력 부분
-    const ButtonContent = (
-        <>
-            {icon && iconPosition === "left" && <span className="mr-2">{icon}</span>}
-            {children}
-            {icon && iconPosition === "right" && <span className="ml-2">{icon}</span>}
-        </>
-    )
+  // 버튼 실제 출력 부분
+  const ButtonContent = (
+    <>
+      {icon && iconPosition === "left" && <span className="mr-2">{icon}</span>}
+      {children}
+      {icon && iconPosition === "right" && <span className="ml-2">{icon}</span>}
+    </>
+  );
 
-    return (
-        <button 
-            type={type}
-            className={classes}
-            disabled={disabled}
-            aria-disabled={disabled}
-            onClick={onClick}
-        >
-            {ButtonContent}
-        </button>
-    )
+  return (
+    <button
+      type={type}
+      className={classes}
+      disabled={disabled}
+      aria-disabled={disabled}
+      onClick={onClick}
+    >
+      {ButtonContent}
+    </button>
+  );
 }
