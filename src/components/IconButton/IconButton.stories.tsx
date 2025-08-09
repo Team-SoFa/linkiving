d42bba8e98cf1a3d01958d83d4c6dea732d4f9e6
Bangdayeon <dybang00@gmail.com>
2025-08-09 15:57:49 +0900

    Fix: modify iconbutton component (#115)

--- src/components/IconButton/IconButton.stories.tsx
+++ src/components/IconButton/IconButton.stories.tsx
@@ -1,37 +1,29 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import IconButton from "./IconButton";
+import type { Meta, StoryObj } from '@storybook/react';
+import IconButton from './IconButton';
+import Image from '../../../public/icons/Image';
 
 const meta: Meta<typeof IconButton> = {
-    title: "Components/IconButton",
-    component: IconButton,
-    tags: ["autodocs"],
-    argTypes: {
-        variant: {
-            control: { type: "radio" },
-            options: ["solid", "outline"],
-        },
-        size: {
-            control: { type: "radio" },
-            options: ["sm", "md", "lg"],
-        },
-        radius: {
-            control: { type: "radio" },
-            options: ["none", "sm", "md", "lg", "full"],
-        },
-        onClick: { action: "clicked" },
-        ariaLabel: { control: 'text' },
+  title: 'Components/IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+  argTypes: {
+    onClick: {
+      description: '아이콘만 가지는 버튼입니다.',
+      action: 'clicked',
     },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof IconButton>;
 
 export const Default: Story = {
-    args: {
-        variant: "solid",
-        size: "md",
-        radius: "md",
-        icon: <img src="/next.svg" className="w-5 h-5" />,
-        ariaLabel: "Icon Button",
-    },
+  args: {
+    variant: 'solid',
+    size: 'md',
+    radius: 'md',
+    icon: <Image src="file.svg" alt="img" />,
+    ariaLabel: 'Icon Button',
+    disabled: false,
+  },
 };
