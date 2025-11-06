0e5e0eb45c50ecc7cca0dcb4d840d4322a0c95b3
Bangdayeon <dybang00@gmail.com>
2025-11-06 19:01:15 +0900

    Chore: add font and color tokens (#184)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import ReactQueryProvider from '@/components/ReactQueryProvider';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 
 import '../styles/globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
