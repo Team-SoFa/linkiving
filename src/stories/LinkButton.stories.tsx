ae06c14c49ccc3d9cea3795a0341f85677b3bbb8
Bangdayeon <dybang00@gmail.com>
2025-11-17 03:42:23 +0900

    Feat: add linkbutton component (#206)

---
+++ src/stories/LinkButton.stories.tsx
@@ -0,0 +1,32 @@
+import LinkButton from '@/components/Button/wrappers/LinkButton/LinkButton';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof LinkButton> = {
+  title: 'Components/Buttons/Wrappers/LinkButton',
+  component: LinkButton,
+  tags: ['autodocs'],
+  argTypes: {
+    href: { control: 'text' },
+    label: { control: 'text' },
+    size: { control: 'inline-radio' },
+    variant: { control: 'inline-radio' },
+    radius: { control: 'inline-radio' },
+    contextStyle: { control: 'inline-radio' },
+    onClick: { action: '버튼 클릭' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof LinkButton>;
+
+export const Default: Story = {
+  args: {
+    href: 'https://google.com',
+    label: '링크 버튼',
+    size: 'md',
+    variant: 'primary',
+    radius: 'md',
+    icon: 'IC_AllLink',
+    contextStyle: 'onMain',
+  },
+};
