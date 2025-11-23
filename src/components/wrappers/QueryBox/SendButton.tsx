5ab34c055bb8df4230d98150fb134fbfdcc31c4a
Bangdayeon <dybang00@gmail.com>
2025-11-24 00:24:56 +0900

    Feat: add querybar component (#154)

---
+++ src/components/wrappers/QueryBox/SendButton.tsx
@@ -0,0 +1,23 @@
+import IconButton from '@/components/basics/IconButton/IconButton';
+
+interface SendButtonProps {
+  disabled: boolean;
+  onClick: () => void;
+}
+const SendButton = ({ disabled, onClick }: SendButtonProps) => {
+  return (
+    <IconButton
+      icon="IC_SendFilled"
+      ariaLabel="메시지 전송 버튼"
+      disabled={disabled}
+      onClick={onClick}
+      size="sm"
+      variant="teritary_subtle"
+      contextStyle="onMain"
+      className="absolute right-0 bottom-0 mr-3 mb-1"
+    />
+  );
+};
+
+SendButton.displayName = 'SendButton';
+export default SendButton;
---
+++ src/stories/QueryBox.stories.tsx
@@ -0,0 +1,20 @@
+import QueryBox from '@/components/wrappers/QueryBox/QueryBox';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Wrappers/QueryBox',
+  component: QueryBox,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof QueryBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    onSubmit: () => {
+      console.log('Submitted!');
+    },
+  },
+};
