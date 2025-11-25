22dbcc303952fe2e8aef3bfb33968accb9d36273
Seong-Myeong <57748493+Seong-Myeong@users.noreply.github.com>
2025-11-26 02:46:17 +0900

    Feat: add Badge component (#88)

---
+++ src/stories/Badge.stories.tsx
@@ -0,0 +1,29 @@
+import Badge from '@/components/basics/Badge/Badge';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Basics/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  args: {
+    label: 'Badge Label',
+    icon: 'IC_SumGenerate',
+  },
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+
+type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {
+  args: {
+    label: 'Badge Label',
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    label: 'Badge Label',
+    icon: 'IC_SumGenerate',
+  },
+};
