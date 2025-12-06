47122a2b8329d513ebfb4b1d4495a48e7e2906b2
Bangdayeon <dybang00@gmail.com>
2025-12-06 17:39:50 +0900

    Chore: update nextjs 16 (#249)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 import { defineConfig } from 'vitest/config';
-
-import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
 
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
@@ -22,11 +20,11 @@ export default defineConfig({
         test: {
           name: 'storybook',
           browser: {
-        enabled: true,
-        headless: true,
-        name: 'chromium',
-        provider: 'playwright'
-      },
+            enabled: true,
+            headless: true,
+            name: 'chromium',
+            provider: 'playwright',
+          },
           setupFiles: ['.storybook/vitest.setup.ts'],
         },
       },
