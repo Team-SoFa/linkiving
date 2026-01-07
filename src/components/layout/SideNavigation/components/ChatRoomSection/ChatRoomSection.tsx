75d8f2ebcc495fe28c810a50253aaaaaaed341aa
Bangdayeon <dybang00@gmail.com>
2026-01-07 10:11:08 +0900

    Feat: add ChatRoomSection (#319)

--- src/components/layout/SideNavigation/components/ChatRoomSection/ChatRoomSection.tsx
+++ src/components/layout/SideNavigation/components/ChatRoomSection/ChatRoomSection.tsx
@@ -12,14 +12,14 @@ const ChatRoomSection = () => {
         <Label className="mb-2 shrink-0">채팅</Label>
         <div className="custom-scrollbar min-h-0 flex-1 overflow-x-hidden overflow-y-auto">
           <ChatItem id={1} label="어쩌구저쩌궁ㄹㅇㄹ아러ㅣ라ㅓ이ㅏ러나ㅣ러나이런아ㅣ런알" />
-          <ChatItem id={1} label="어쩌구저쩌궁ㄹㅇㄹ아러ㅣ라ㅓ이ㅏ러나ㅣ러나이런아ㅣ런알" />
-          <ChatItem id={1} label="어쩌구저쩌궁ㄹㅇㄹ아러ㅣ라ㅓ이ㅏ러나ㅣ러나이런아ㅣ런알" />
-          <ChatItem id={1} label="어쩌구저쩌궁ㄹㅇㄹ아러ㅣ라ㅓ이ㅏ러나ㅣ러나이런아ㅣ런알" />
+          <ChatItem id={2} label="어쩌구저쩌궁ㄹㅇㄹ아러ㅣ라ㅓ이ㅏ러나ㅣ러나이런아ㅣ런알" />
+          <ChatItem id={3} label="어쩌구저쩌궁ㄹㅇㄹ아러ㅣ라ㅓ이ㅏ러나ㅣ러나이런아ㅣ런알" />
+          <ChatItem id={4} label="어쩌구저쩌궁ㄹㅇㄹ아러ㅣ라ㅓ이ㅏ러나ㅣ러나이런아ㅣ런알" />
         </div>
       </div>
 
-      {type === 'DELETE_CHAT' && props && 'chatId' in props && (
-        <DeleteChatModal chatId={props.chatId as number} />
+      {type === 'DELETE_CHAT' && typeof props?.chatId === 'number' && (
+        <DeleteChatModal chatId={props.chatId} />
       )}
     </>
   );
