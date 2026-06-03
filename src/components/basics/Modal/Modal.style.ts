e155395817ef17394d96100e3b5fb4f81b241a2d
Bangdayeon <dybang00@gmail.com>
2026-06-03 23:11:20 +0900

    Design: mobile modal bottomsheet (#498)

--- src/components/basics/Modal/Modal.style.ts
+++ src/components/basics/Modal/Modal.style.ts
@@ -2,11 +2,11 @@
 import { tv } from 'tailwind-variants';
 
 export const modalOverlayStyle = tv({
-  base: 'fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.5)]',
+  base: 'fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.5)] max-md:items-end',
 });
 
 export const modalContentStyle = tv({
-  base: 'bg-gray50 relative h-auto w-auto rounded-2xl shadow-lg',
+  base: 'bg-gray50 max-md:modal-slide-up relative h-auto w-auto rounded-2xl shadow-lg max-md:m-0! max-md:w-full! max-md:max-w-none! max-md:min-w-0! max-md:rounded-b-none!',
 });
 
 export const modalHeaderStyle = tv({
