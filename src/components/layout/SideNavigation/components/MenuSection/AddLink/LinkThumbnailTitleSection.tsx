91a33f463c2a7faad5cf3893362c40a7ff86eba5
Bangdayeon <dybang00@gmail.com>
2026-06-21 16:38:41 +0900

    Design: add link modal label (#533)

--- src/components/layout/SideNavigation/components/MenuSection/AddLink/LinkThumbnailTitleSection.tsx
+++ src/components/layout/SideNavigation/components/MenuSection/AddLink/LinkThumbnailTitleSection.tsx
@@ -4,6 +4,7 @@ import Spinner from '@/components/basics/Spinner/Spinner';
 import TextArea from '@/components/basics/TextArea/TextArea';
 import { MAX_TITLE_LENGTH } from '@/lib/constants/link';
 import Image from 'next/image';
+import { useState } from 'react';
 import { Control, Controller, FieldErrors } from 'react-hook-form';
 
 import { AddLinkForm } from './hooks/useAddLinkForm';
@@ -25,52 +26,57 @@ export default function LinkThumbnailTitleSection({
   shouldDisableDetails,
   previewImageUrl,
 }: Props) {
+  const [isTitleFocused, setIsTitleFocused] = useState(false);
+
   return (
-    <div className="flex flex-col gap-2 md:flex-row">
-      <div className="flex flex-1 flex-col">
-        <Label>썸네일</Label>
-        <div
-          className={`relative h-30 w-full overflow-hidden rounded-lg bg-white md:h-[4.2rem] ${
-            shouldDisableDetails ? 'opacity-60' : ''
-          }`}
-          aria-disabled={shouldDisableDetails}
-        >
-          {metaLoading && isValidUrl ? (
-            <>
-              <Skeleton className="h-full w-full" radius="lg" animated />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <Spinner size="lg" />
-              </div>
-            </>
-          ) : (
-            <Image src={previewImageUrl} alt="link thumbnail" fill />
+    <div className="flex flex-col gap-2">
+      <Label>링크 정보</Label>
+      <div className="flex flex-col gap-2 md:flex-row">
+        <div className="flex flex-1">
+          <div
+            className={`relative h-60 w-full overflow-hidden rounded-lg bg-white md:h-[4.2rem] ${
+              shouldDisableDetails ? 'opacity-60' : ''
+            }`}
+            aria-disabled={shouldDisableDetails}
+          >
+            {metaLoading && isValidUrl ? (
+              <>
+                <Skeleton className="h-full w-full" radius="lg" animated />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <Spinner size="lg" />
+                </div>
+              </>
+            ) : (
+              <Image src={previewImageUrl} alt="link thumbnail" fill />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-3 flex-col">
+          <Controller
+            name="title"
+            control={control}
+            render={({ field }) => (
+              <TextArea
+                {...field}
+                placeholder="제목을 입력해 주세요."
+                id="title-input"
+                radius="lg"
+                heightLines={2}
+                maxHeightLines={2}
+                showMax={isTitleFocused}
+                onFocus={() => setIsTitleFocused(true)}
+                maxLength={MAX_TITLE_LENGTH}
+                isLoading={metaLoading && isValidUrl}
+                disabled={shouldDisableDetails}
+                value={field.value ?? ''}
+                onChange={e => field.onChange(e)}
+              />
+            )}
+          />
+          {errors.title && !shouldDisableDetails && (
+            <span className="text-red500 text-xs">{errors.title.message as string}</span>
           )}
         </div>
-      </div>
-      <div className="flex flex-3 flex-col">
-        <Label htmlFor="title-input">제목</Label>
-        <Controller
-          name="title"
-          control={control}
-          render={({ field }) => (
-            <TextArea
-              {...field}
-              placeholder="제목을 입력해 주세요."
-              id="title-input"
-              radius="lg"
-              heightLines={2}
-              maxHeightLines={2}
-              maxLength={MAX_TITLE_LENGTH}
-              isLoading={metaLoading && isValidUrl}
-              disabled={shouldDisableDetails}
-              value={field.value ?? ''}
-              onChange={e => field.onChange(e)}
-            />
-          )}
-        />
-        {errors.title && !shouldDisableDetails && (
-          <span className="text-red500 text-xs">{errors.title.message as string}</span>
-        )}
       </div>
     </div>
   );
